fb39572c8f97f8d9129c8530c01e162957a213b5
bae080311 <s24024@gsm.hs.kr>
2024-10-21 15:16:04 +0900

    feat: placeholder

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -13,6 +13,7 @@ export function Header() {
       <div className="relative">
         <input
           type="text"
+          placeholder="찾고 싶은 물건을 입력해보세요!"
           className="h-[34px] rounded-lg bg-primary-bg w-[600px] pl-10"
         />
         <svg
