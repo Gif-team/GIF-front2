65b6087e139f1869ccaf4aab8939681f1a72dc96
bae080311 <s24024@gsm.hs.kr>
2024-10-21 10:55:44 +0900

    fix: routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Main } from "./pages/main";
+import { Profile } from "./pages/profile";
+import { Post } from "./pages/post";
+import { Writing } from "./pages/writing";
+import { Login } from "./pages/login";
+import { SignUp } from "./pages/signup";
+import { Badge } from "./pages/badge";
+import { Chating } from "./pages/chating";
+import { NotFound } from "./pages/notFound";
+import { Starting } from "./pages/starting";
 
 function App() {
   return (
@@ -10,7 +20,7 @@ function App() {
         <Route path="/posts" element={<Post />} />
         <Route path="/posts/writes" element={<Writing />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/users/signup" element={<Signup />} />
+        <Route path="/users/signup" element={<SignUp />} />
         <Route path="/badge" element={<Badge />} />
         <Route path="/chating" element={<Chating />} />
         <Route path="*" element={<NotFound />} />
