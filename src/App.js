bc1d643e09b36ffdba4d3aee9fa1ba5358a44b74
bae080311 <s24024@gsm.hs.kr>
2024-10-21 23:17:45 +0900

    feat: context

--- src/App.js
+++ src/App.js
@@ -9,23 +9,26 @@ import { Badge } from "./pages/badge";
 import { Chating } from "./pages/chating";
 import { NotFound } from "./pages/notFound";
 import { Starting } from "./pages/starting";
+import { AlertProvider } from "./context/alertContext";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/starting" element={<Starting />} />
-        <Route path="/" element={<Main />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/posts" element={<Post />} />
-        <Route path="/posts/writes" element={<Writing />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/users/signup" element={<SignUp />} />
-        <Route path="/badge" element={<Badge />} />
-        <Route path="/chating" element={<Chating />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <AlertProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/starting" element={<Starting />} />
+          <Route path="/" element={<Main />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/posts" element={<Post />} />
+          <Route path="/posts/writes" element={<Writing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/users/signup" element={<SignUp />} />
+          <Route path="/badge" element={<Badge />} />
+          <Route path="/chating" element={<Chating />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </AlertProvider>
   );
 }
 
