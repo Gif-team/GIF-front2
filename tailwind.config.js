51cc36cf22f054cc62e1ce8d1c7e707cfed36866
bae080311 <s24024@gsm.hs.kr>
2024-10-21 10:36:40 +0900

    feat: tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          primary: "#615EFC",
+          second: "#7E8EF1",
+          errorcolor: "#DF454A",
+        },
+        gray: {
+          700: "#E9E9E9",
+          400: "#C4C4C4",
+          300: "#F0F0F0",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
