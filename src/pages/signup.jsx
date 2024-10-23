59113311a3444fe28323393ddf4a015b6f30f88f
bae080311 <s24024@gsm.hs.kr>
2024-10-23 17:13:01 +0900

    feat: ✨ state

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,3 +1,58 @@
+import React, { useState } from "react";
+import blueLogo from "../imgs/blueLogo.png";
+
 export function SignUp() {
-  return <h1>Sign Up</h1>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordCheck, setPasswordCheck] = useState("");
+
+  return (
+    <div className="flex items-center justify-center">
+      <div className="flex flex-col items-center p-4 border rounded-md border-gray-login">
+        <img src={blueLogo} className="w-[100px]" alt="로고" />
+
+        <div className="flex flex-col">
+          <label htmlFor="email">이메일</label>
+          <input
+            id="email"
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="w-[360px] h-[40px] border border-gray-login"
+          />
+        </div>
+
+        <div className="flex flex-col">
+          <label htmlFor="password">비밀번호</label>
+          <input
+            id="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="w-[360px] h-[40px] border border-gray-login"
+          />
+        </div>
+
+        <div className="flex flex-col">
+          <label htmlFor="passwordCheck">비밀번호 확인</label>
+          <input
+            id="passwordCheck"
+            type="password"
+            value={passwordCheck}
+            onChange={(e) => setPasswordCheck(e.target.value)}
+            className="w-[360px] h-[40px] border border-gray-login"
+          />
+        </div>
+        <button
+          className={`text-white mt-4 ${
+            password && email && passwordCheck
+              ? "bg-primary-primary"
+              : "bg-primary-second"
+          }`}
+        >
+          회원가입
+        </button>
+      </div>
+    </div>
+  );
 }
