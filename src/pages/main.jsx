74547da82863dbfd493f394feafa922f6901b42d
bae080311 <s24024@gsm.hs.kr>
2024-10-23 10:10:56 +0900

    fix: 💄게시물 정렬

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -14,42 +14,42 @@ export function Main() {
   return (
     <div className="flex flex-col items-center w-full ">
       <Header />
-      <div className="w-[60%] flex">
-        <h1 className="m-5 text-2xl font-bold">최신 게시물</h1>
-        {open ? (
-          <>
-            <div className="fixed top-0 left-0 z-10 w-full h-full bg-black bg-opacity-30" />
-            <div className=" fixed right-0 h-[100%] transform w-1/3 bg-white p-5 z-20">
-              <div className="flex items-start justify-between">
-                <h5 className="text-lg font-semibold">알람</h5>
-                <svg
-                  onClick={() => setAlertPopUp(!alertPopUp)}
-                  className="cursor-pointer"
-                  width="32"
-                  height="32"
-                  viewBox="0 0 32 32"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <g id="close">
-                    <path
-                      id="Path 2"
-                      d="M23.5425 23.5424L8.45758 8.45746"
-                      stroke="#7E8EF1"
-                      strokeLinecap="round"
-                    />
-                    <path
-                      id="Path 2_2"
-                      d="M23.5424 8.45746L8.45747 23.5424"
-                      stroke="#7E8EF1"
-                      strokeLinecap="round"
-                    />
-                  </g>
-                </svg>
-              </div>
+      {open ? (
+        <>
+          <div className="fixed top-0 left-0 z-10 w-full h-full bg-black bg-opacity-30" />
+          <div className=" fixed right-0 h-[100%] transform w-1/3 bg-white p-5 z-20">
+            <div className="flex items-start justify-between">
+              <h5 className="text-lg font-semibold">알람</h5>
+              <svg
+                onClick={() => setAlertPopUp(!alertPopUp)}
+                className="cursor-pointer"
+                width="32"
+                height="32"
+                viewBox="0 0 32 32"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g id="close">
+                  <path
+                    id="Path 2"
+                    d="M23.5425 23.5424L8.45758 8.45746"
+                    stroke="#7E8EF1"
+                    strokeLinecap="round"
+                  />
+                  <path
+                    id="Path 2_2"
+                    d="M23.5424 8.45746L8.45747 23.5424"
+                    stroke="#7E8EF1"
+                    strokeLinecap="round"
+                  />
+                </g>
+              </svg>
             </div>
-          </>
-        ) : null}
+          </div>
+        </>
+      ) : null}
+      <div className="w-[60%] flex flex-wrap">
+        <h1 className="m-5 text-2xl font-bold">최신 게시물</h1>
       </div>
       <div className="fixed flex flex-col gap-4 bottom-6 right-16">
         <svg
