5c3a6ec50d03db18fc8273a9d0fb58e5b17c8ca4
bae080311 <s24024@gsm.hs.kr>
2024-10-23 17:23:41 +0900

    fix: form

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -5,43 +5,43 @@ export function SignUp() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passwordCheck, setPasswordCheck] = useState("");
-
   return (
     <div className="flex items-center justify-center">
-      <div className="flex flex-col items-center p-4 border rounded-md border-gray-login">
-        <img src={blueLogo} className="w-[100px]" alt="로고" />
+      <div className="flex flex-col items-center justify-between gap-8 p-4 border rounded-md border-gray-login">
+        <img src={blueLogo} className="w-[90px]" alt="로고" />
+        <div className="flex flex-col justify-between gap-3">
+          <div className="flex flex-col">
+            <label htmlFor="email">이메일</label>
+            <input
+              id="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-[360px] h-[40px] border border-gray-login rounded-lg"
+            />
+          </div>
 
-        <div className="flex flex-col">
-          <label htmlFor="email">이메일</label>
-          <input
-            id="email"
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="w-[360px] h-[40px] border border-gray-login rounded-lg"
-          />
-        </div>
+          <div className="flex flex-col ">
+            <label htmlFor="password">비밀번호</label>
+            <input
+              id="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-[360px] h-[40px] border border-gray-login rounded-lg"
+            />
+          </div>
 
-        <div className="flex flex-col ">
-          <label htmlFor="password">비밀번호</label>
-          <input
-            id="password"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="w-[360px] h-[40px] border border-gray-login rounded-lg"
-          />
-        </div>
-
-        <div className="flex flex-col">
-          <label htmlFor="passwordCheck">비밀번호 확인</label>
-          <input
-            id="passwordCheck"
-            type="password"
-            value={passwordCheck}
-            onChange={(e) => setPasswordCheck(e.target.value)}
-            className="w-[360px] h-[40px] border border-gray-login rounded-lg"
-          />
+          <div className="flex flex-col">
+            <label htmlFor="passwordCheck">비밀번호 확인</label>
+            <input
+              id="passwordCheck"
+              type="password"
+              value={passwordCheck}
+              onChange={(e) => setPasswordCheck(e.target.value)}
+              className="w-[360px] h-[40px] border border-gray-login rounded-lg"
+            />
+          </div>
         </div>
         <button
           className={`text-white mt-4 px-36 py-2 rounded-lg font-semibold ${
