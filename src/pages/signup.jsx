35d7d9d2df750a633cd7e73e7144df21ab438009
bae080311 <s24024@gsm.hs.kr>
2024-10-24 09:20:27 +0900

    feat: ✨유효성 검사

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import blueLogo from "../imgs/blueLogo.png";
 
+const emailregex = /^[a-zA-Z0-9._%+-]+@gsm\.hs\.kr$/i;
+const passwordregex = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9]).{8,}$/;
+
 export function SignUp() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [passwordCheck, setPasswordCheck] = useState("");
+  const [email, setEmail] = useState(false);
+  const [password, setPassword] = useState(false);
+  const [passwordCheck, setPasswordCheck] = useState(false);
+  const [passwordValue, setPasswordValue] = useState("");
   return (
     <div className="flex items-center justify-center">
       <div className="flex flex-col items-center mt-[120px] justify-between gap-8 p-8 border rounded-lg border-gray-login">
@@ -18,10 +22,14 @@ export function SignUp() {
               id="email"
               placeholder="학교 이메일을 입력해주세요..."
               type="text"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="text-[12px] p-3 w-[360px] h-[40px] border border-gray-login rounded-lg"
+              onChange={(e) =>
+                e.target.value.match(emailregex)
+                  ? setEmail(false)
+                  : setEmail(true)
+              }
+              className="text-[14px] focus:border-primary-primary p-3 w-[360px] h-[40px] border border-gray-login rounded-lg"
             />
+            {email ? <div>올바른 이메일 주소를 입력하세요</div> : null}
           </div>
 
           <div className="flex flex-col ">
@@ -30,12 +38,20 @@ export function SignUp() {
             </label>
             <input
               id="password"
-              placeholder="영문, 숫자, 특수문자 2개 이상 포함 8글자 이상"
+              placeholder="영문, 숫자, 특수문자 포함 8글자 이상"
               type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="text-[12px] p-3 w-[360px] h-[40px] border border-gray-login rounded-lg"
+              onChange={(e) =>
+                e.target.value.match(passwordregex)
+                  ? (setPassword(false), setPasswordValue(e.target.value))
+                  : setPassword(true)
+              }
+              className="text-[14px] p-3 w-[360px] focus:border-primary-primary h-[40px] border border-gray-login rounded-lg"
             />
+            {password ? (
+              <div>
+                영문, 숫자, 특수문자를 포함하여 8글자 이상으로 적어주세요
+              </div>
+            ) : null}
           </div>
 
           <div className="flex flex-col">
@@ -46,10 +62,14 @@ export function SignUp() {
               id="passwordCheck"
               type="password"
               placeholder="비밀번호를 재입력해주세요..."
-              value={passwordCheck}
-              onChange={(e) => setPasswordCheck(e.target.value)}
-              className=" text-[12px] p-3 w-[360px] h-[40px] border border-gray-login rounded-lg"
+              onChange={(e) =>
+                e.target.value === passwordValue
+                  ? setPasswordCheck(false)
+                  : setPasswordCheck(true)
+              }
+              className=" text-[14px] p-3 w-[360px] h-[40px] border border-gray-login rounded-lg"
             />
+            {passwordCheck ? <div>비밀번호가 일치하지 않아요</div> : null}
           </div>
         </div>
         <button
